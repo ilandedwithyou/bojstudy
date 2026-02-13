f3e3c40d56e86fa4f8e9b5b3d96229743b54f2b9
ilandedwithyou <ilandedwithyou@gmail.com>
2026-02-13 22:08:48 +0900

    15650

---
+++ Silver III/15650.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+arr = []
+
+def dfs(start):
+    if len(arr) == M:
+        print(*arr)
+        return
+
+    for i in range(start, N+1):
+        arr.append(i)
+        dfs(i+1)
+        arr.pop()
+
+dfs(1)
